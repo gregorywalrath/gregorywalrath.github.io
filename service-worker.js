cb202f4d30edf630e9e81f2737acca1b811d65dc
Greg Walrath <87130862+gregorywalrath@users.noreply.github.com>
2024-12-05 15:46:33 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = "basketball-stats-cache-v1";
-const urlsToCache = [
+const VERSION = "v1";
+const CACHE_NAME = `basketball-stats-cache-${VERSION}`;
+const APP_STATIC_RESOURCES = [
     "./",
     "./index.html",
     "./bootstrap-5.3.3-dist/css/bootstrap.css",
@@ -7,19 +8,50 @@ const urlsToCache = [
     "./manifest.json",
     "./icon-192x192.png",
 ];
-
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(urlsToCache);
-        })
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll(APP_STATIC_RESOURCES);
+        })(),
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                }),
+            );
+            await clients.claim();
+        })(),
     );
 });
 
 self.addEventListener("fetch", (event) => {
+    // when seeking an HTML page
+    if (event.request.mode === "navigate") {
+        // Return to the index.html page
+        event.respondWith(caches.match("/"));
+        return;
+    }
+
+    // For every other request type
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(event.request.url);
+            if (cachedResponse) {
+                // Return the cached response if it's available.
+                return cachedResponse;
+            }
+            // Respond with a HTTP 404 response status.
+            return new Response(null, { status: 404 });
+        })(),
     );
 });
